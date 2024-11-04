4396c1ac368106f8c4d0b4f5b4dc539130aeefcd
setayesh karbasi <karbasisetayesh7@gmail.com>
2024-11-04 15:19:38 +0100

    Initial Commit

---
+++ setayeshhh/setayeshhhApp.swift
@@ -0,0 +1,17 @@
+//
+//  setayeshhhApp.swift
+//  setayeshhh
+//
+//  Created by setayesh karbasi on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct setayeshhhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
